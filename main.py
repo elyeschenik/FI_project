0416d66711a05e5edbc234ea1d32fbaa7e8a86f6
Elyès Chenik <elyes.chenik@gmail.com>
2021-05-05 01:30:24 +0200

    Finished part A

--- main.py
+++ main.py
@@ -2,12 +2,13 @@ from deposit import *
 from fra import *
 from swap import *
 from convention import *
+from swaption import *
 
-type_of_product = input("What product do you want to price (type \"Deposit\", \"FRA\" or \"Swap\")?")
+type_of_product = input("What product do you want to price (type \"Deposit\", \"FRA\" \"Swap\", \"PSwaption\" or \"CSSwaption\")?")
 
 pricing_date = datetime(2021,4,5)
-start_date = datetime(2021,4,5)
-end_date = datetime(2022,10,2)
+start_date = datetime(2021,9,5)
+end_date = datetime(2023,9,5)
 
 curve_1 = pd.read_excel("curves.xlsx", sheet_name="OIS Curve for discounting", index_col=0)
 curve_2 = pd.read_excel("curves.xlsx", sheet_name="USD3M - Curve for forwards", index_col=0)
@@ -15,7 +16,7 @@ curve_2 = pd.read_excel("curves.xlsx", sheet_name="USD3M - Curve for forwards", 
 
 convention = convention(365, "aasba", "aasba")
 
-notional = 100
+notional = 1000000
 
 ##############################DEPOSIT##############################
 if type_of_product == "Deposit":
@@ -45,3 +46,32 @@ if type_of_product == "Swap":
     SWAP = Swap(pricing_date, start_date, end_date, curve_1, curve_2, isPayer, fixed_rate, fixed_freq, float_freq, convention, notional)
     print("The swap present value is {:.2f}$".format(round(SWAP.PV(), 2)))
     print("The swap par rate is {}%".format(SWAP.Get_par_rate()))
+
+###############################Physical SWAPTION################################
+if type_of_product == "PSwaption":
+
+    isPayer = True
+    fixed_freq = 1 #per year
+    float_freq = 2
+
+    vol = 0.06
+    strike = 0.01
+    expiry = start_date
+
+    PSWAPTION = Physical_Swaption(pricing_date, start_date, end_date, expiry, curve_1, curve_2, isPayer, strike, fixed_freq, float_freq, vol, convention, notional)
+    print("The physical swaption present value is {:.2f}$".format(round(PSWAPTION.PV(), 2)))
+
+###############################Physical SWAPTION################################
+if type_of_product == "CSSwaption":
+
+    isPayer = True
+    fixed_freq = 1 #per year
+    float_freq = 2
+
+    vol = 0.06
+    strike = 0.01
+    expiry = start_date
+
+    CSSWAPTION = Cash_Settled_Swaption(pricing_date, start_date, end_date, expiry, curve_1, curve_2, isPayer, strike, fixed_freq, float_freq, vol, convention, notional)
+    print("The cash settled swaption present value is {:.2f}$".format(round(CSSWAPTION.PV(), 2)))
+
--- product.py
+++ product.py
@@ -2,6 +2,7 @@ from abc import abstractmethod
 import pandas as pd
 import numpy as np
 from datetime import datetime, timedelta
+from scipy.stats import norm
 
 class Product:
 
@@ -54,7 +55,7 @@ class Product:
 
         return discount_factor
 
-    def get_Forward(self, date = None, curve_num=1):
+    def get_Rate(self, date = None, curve_num=1):
 
         DF = self.get_DF(date, curve_num)
         forward_rate = -np.log(DF)/self.coverage(self.pricing_date, date)
@@ -68,35 +69,13 @@ class Product:
 
         return (1/delta) * ((B_1/B_2) - 1)
 
-    """
-    def get_DF_bis(self, date = None):
-
-        discount_rate = self.get_rate(date)
-        delta = self.coverage(self.pricing_date, date)
-
-        return np.exp(-discount_rate * delta)
-    """
-
-    """
-        def get_rate(self, date = None):
-
-        if date is None:
-            date = self.end_date
-
-        if date == self.pricing_date:
-            return 0
-
-        if date in self.curve_1.index:
-            discount_rate = self.curve_1.loc[date].values[0]
+    def BSClosedForm(self, S_0, K, r, sigma, T, isCall):
+        d1 = (np.log(S_0 / K) + (r + 0.5 * sigma ** 2) * T) / (sigma * np.sqrt(T))
+        d2 = d1 - sigma * np.sqrt(T)
+        if isCall:
+            return S_0 * norm.cdf(d1) - np.exp(-r * T) * K * norm.cdf(d2)
         else:
-            d_bottom = self.curve_1.loc[self.curve_1.index < date].index[-1]
-            d_up = self.curve_1.loc[self.curve_1.index > date].index[0]
+            return -S_0 * norm.cdf(-d1) + np.exp(-r * T) * K * norm.cdf(-d2)
 
-            self.curve_1.loc[date] = None
 
-            discount_rate = self.curve_1.sort_index().loc[[d_bottom, date, d_up]].interpolate(method='time').loc[date].values[0]
-
-            self.curve_1.drop(date, axis = 0, inplace = True)
-
-        return discount_rate
-    """
+   
